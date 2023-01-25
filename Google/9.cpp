7ff3d9c3927a0e1f0750d90a4836b2820728c0b2
singhs124 <94396164+singhs124@users.noreply.github.com>
2023-01-25 20:24:00 +0530

    Create 9.cpp

---
+++ Google/9.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin() , piles.end()) ;
+        int n = piles.size() ;
+        int res = 0 ;
+        for(int i = n/3 ; i < n ; i+=2) res += piles[i] ;
+        return res ;
+    }
+};
